cac530566ae63245941ab43f0f0c1b61178da331
Alphadrake86 <anderson8221@student.cptc.edu>
2020-08-06 15:09:32 -0700

    added the CUD

--- ComputerHardwareInventoryManager/DB Classes/HardwareDB.cs
+++ ComputerHardwareInventoryManager/DB Classes/HardwareDB.cs
@@ -14,5 +14,25 @@ namespace ComputerHardwareInventoryManager.DB_Classes
 
             return Hcon.HardwareProducts.ToList();
         }
+
+        public static void Insert(HardwareProduct HP)
+        {
+            HardwareContext Hcon = new HardwareContext();
+            Hcon.HardwareProducts.Add(HP);
+        }
+
+        public static void Update(HardwareProduct old, HardwareProduct HP)
+        {
+            HardwareContext Hcon = new HardwareContext();
+            Hcon.HardwareProducts.Remove(old);
+            Hcon.HardwareProducts.Add(HP);
+
+        }
+
+        public static void Delete(HardwareProduct HP)
+        {
+            HardwareContext Hcon = new HardwareContext();
+            Hcon.HardwareProducts.Remove(HP);
+        }
     }
 }
