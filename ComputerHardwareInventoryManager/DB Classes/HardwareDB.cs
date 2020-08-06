f5a1f4e8b7682716a62768437729d948e2ab9fb8
Alphadrake86 <anderson8221@student.cptc.edu>
2020-08-06 15:02:04 -0700

    Added HArdwareDB class and Retrieve

---
+++ ComputerHardwareInventoryManager/DB Classes/HardwareDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerHardwareInventoryManager.DB_Classes
+{
+    static class HardwareDB
+    {
+        public static List<HardwareProduct> GetHardwareProducts()
+        {
+            HardwareContext Hcon = new HardwareContext();
+
+            return Hcon.HardwareProducts.ToList();
+        }
+    }
+}
